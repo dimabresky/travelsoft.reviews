e1a7f9351845617aa465987deb737a26e3400d7c
dimabresky <dimabresky@gmail.com>
2017-11-09 23:33:56 +0300

    reviews.add component is updated

--- install/components/reviews.add/class.php
+++ install/components/reviews.add/class.php
@@ -45,18 +45,25 @@ class TravelsoftReviewsAdd extends CBitrixComponent {
                         $this->arResult["ERRORS"]["register_fail"] = $result['MESSAGE'];
                         
                     }
+                } else {
+                    
+                    $result = $USER->Login($_POST['email'], $_POST['password'], "Y");
+                    
+                    if ($result !== true) {
+                        $this->arResult["ERRORS"]["auth_fail"] = $result['MESSAGE'];
+                    }
                 }
             }
             
+            $review = trim(strip_tags($_POST['review']));
+                
+            if (strlen($review) == 0) {
+
+                $this->arResult['ERRORS']['empty_review'] = '';
+
+            }
+            
             if (empty($this->arResult['ERRORS'])) {
-                
-                $review = trim(strip_tags($_POST['review']));
-                
-                if (strlen($review) == 0) {
-                    
-                    $this->arResult['ERRORS']['empty_review'] = '';
-                    
-                }
                 
                 $limitations = '';
                 if ($this->arParams['SHOW_LIMITATIONS_FIELD'] === 'Y') {
@@ -74,6 +81,42 @@ class TravelsoftReviewsAdd extends CBitrixComponent {
                 if ($this->arParams['SHOW_RATING_FIELD'] === 'Y') {
                     
                     $rating = (int)trim(strip_tags($_POST['rating']));
+                }
+                
+                $images = array();
+                if ($this->arParams['SHOW_IMAGES_FIELD'] === 'Y') {
+                    
+                    $images = $_FILES['images'];
+                }
+                
+                if (empty($this->arResult['ERRORS'])) {
+                    
+                    $el = new CIBLockElemet;
+                    
+                    $result = $el->Add(array(
+                        "NAME" => "Review_" . date('d.m.Y H:s:i'),
+                        "ACTIVE" => $this->arParams['NEED_PREMODERATION'] === 'Y' ? 'Y' : 'N',
+                        "DETAIL_TEXT" => $review,
+                        "CODE" => "review_" . time(),
+                        "PROPERTY_VALUES" => array(
+                            "USER_ID" => $USER->GetID(),
+                            "USER_NAME" => $USER->GetFullName(),
+                            "USER_EMAIL" => $USER->GetEmail(),
+                            "RATING" => $rating,
+                            "ADVANTAGES" => $advantages,
+                            "LIMITATIONS" => $limitations,
+                            "PICTURES" => $images
+                        )
+                    ));
+                    
+                    if ($result) {
+                        
+                        
+                        
+                        LocalRedirect($APPLICATION->GetCurPageParam("", array(), false));
+                    }
+                    
+                    $this->arResult['ERRORS']['review_add_fail'] = $el->LAST_ERROR;
                 }
             }
             
